a87f9fbb21b2b1d48790725d1e5eec8d5fb8f868
timmy <std@stdx.de>
2012-02-01 16:08:15 +0100

    Implemented game logic.

--- rocks.rb
+++ rocks.rb
@@ -14,4 +14,15 @@ get '/throw/:type' do
     halt 403, "Invalid throw. You must throw one of the following: #{@throws}"
   end
   
+  # let the computer throw randomly
+  computer_throw = @throws.sample
+
+  if player_throw == computer_throw then
+    "You tied with the computer. Lets try again."
+  elsif @defeats[player_throw] == computer_throw then
+    "Your throw #{player_throw} beats #{computer_throw}. You won against the computer."
+  else
+    "Your throw #{player_throw} was beaten by #{computer_throw}. You lost against the computer."
+  end
+  
 end
