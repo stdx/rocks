30b3de5afbe2d475084d106213ba1d09aa69af70
timmy <std@stdx.de>
2012-02-01 16:02:30 +0100

    Added init function for game.

--- rocks.rb
+++ rocks.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
 
+before do
+  content_type :txt
+  @defeats = {rock: :scissors, paper: :rock, scissors: :paper}
+  @throws = @defeats.keys
+end
+
+
 get '/throw/:type' do
   # play here
 end
