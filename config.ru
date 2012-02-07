abdc33ace2bb864b63788b69862f7e39853c34bd
timmy <std@stdx.de>
2012-02-07 20:06:03 +0100

    Created a Gemfile and .rvmrc.
    * Added Gemfile and config.ru
    * Added rvm configuration file.
    * Trusted the .rvmrc file
    * Added sinatra gem to Gemfile
    * Configured Application.
    * Added thin to Gemfile
    * Added the Gemfile.lock.

---
+++ config.ru
@@ -0,0 +1,8 @@
+require 'rubygems'
+require 'bundler'
+
+Bundler.require
+
+require './rocks'
+
+run Rocks
--- rocks.rb
+++ rocks.rb
@@ -1,28 +1,29 @@
-require 'sinatra'
 
-before do
-  content_type :txt
-  @defeats = {rock: :scissors, paper: :rock, scissors: :paper}
-  @throws = @defeats.keys
-end
+class Rocks < Sinatra::Base
 
-
-get '/throw/:type' do
-  player_throw = params[:type].to_sym
-  
-  if !@throws.include?(player_throw) then
-    halt 403, "Invalid throw. You must throw one of the following: #{@throws}"
+  before do
+    content_type :txt
+    @defeats = {rock: :scissors, paper: :rock, scissors: :paper}
+    @throws = @defeats.keys
   end
-  
-  # let the computer throw randomly
-  computer_throw = @throws.sample
 
-  if player_throw == computer_throw then
-    "You tied with the computer. Lets try again."
-  elsif @defeats[player_throw] == computer_throw then
-    "Your throw #{player_throw} beats #{computer_throw}. You won against the computer."
-  else
-    "Your throw #{player_throw} was beaten by #{computer_throw}. You lost against the computer."
+  get '/throw/:type' do
+    player_throw = params[:type].to_sym
+    
+    if !@throws.include?(player_throw) then
+      halt 403, "Invalid throw. You must throw one of the following: #{@throws}"
+    end
+    
+    # let the computer throw randomly
+    computer_throw = @throws.sample
+
+    if player_throw == computer_throw then
+      "You tied with the computer. Lets try again."
+    elsif @defeats[player_throw] == computer_throw then
+      "Your throw #{player_throw} beats #{computer_throw}. You won against the computer."
+    else
+      "Your throw #{player_throw} was beaten by #{computer_throw}. You lost against the computer."
+    end
   end
-  
+
 end
